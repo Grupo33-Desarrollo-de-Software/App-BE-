a6fabf7c2dc4dfe3275288440b91b4cc44065d56
unknown <mateoedgardogomez@gmail.com>
2024-11-08 19:26:00 -0300

    Creado modelo Artista

--- Notify/artistas/models.py
+++ Notify/artistas/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Artista(models.Model):
+    name = models.CharField(max_length=50)
+    image = models.CharField(max_length=300) #Es un charfield ya que refiere al link de la imagen
+    listeners = models.PositiveBigIntegerField(null=True)
+    plays = models.PositiveBigIntegerField(null=True)
+    summary = models.TextField(null=True)
