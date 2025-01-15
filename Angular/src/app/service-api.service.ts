ae1012d9fbaaebb54f6d30885beef2070d38a7fb
nic01aszzz <nicopiccart@hotmail.com>
2025-01-15 19:13:16 -0300

    Avanzado mostrar la FL de un usuario

--- Angular/src/app/service-api.service.ts
+++ Angular/src/app/service-api.service.ts
@@ -24,3 +24,5 @@ export class ServiceAPI {
     ) as Observable<Album>;
   }
 }
+
+ 
--- Notify/Notify/urls.py
+++ Notify/Notify/urls.py
@@ -20,20 +20,21 @@ from django.views.generic.base import RedirectView
 from rest_framework.routers import DefaultRouter
 from usuarios.views import UserViewSet, UserLogIn
 
-"""
+
 urlpatterns = [
    path('api/v1/', include('albums.urls')),
-    path(r'admin/', admin.site.urls),
+   path('api/v1/', include('followlists.urls')),
+   path(r'admin/', admin.site.urls),
 ]
-"""
 
-router = DefaultRouter()
-router.register(r'users', UserViewSet)
 
-urlpatterns = [
-    path(r'admin/', admin.site.urls),
-    path('api/v1/', include(router.urls)),
-    path('api-user-login/', UserLogIn.as_view()),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    re_path(r'^$', RedirectView.as_view(url=reverse_lazy('api-root'), permanent=False)),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# router = DefaultRouter()
+# router.register(r'users', UserViewSet)
+
+# urlpatterns = [
+#     path(r'admin/', admin.site.urls),
+#     path('api/v1/', include(router.urls)),
+#     path('api-user-login/', UserLogIn.as_view()),
+#     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+#     re_path(r'^$', RedirectView.as_view(url=reverse_lazy('api-root'), permanent=False)),
+# ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
