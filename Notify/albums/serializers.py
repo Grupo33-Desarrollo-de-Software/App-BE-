8b180d4c24800715e064f8e864e30726cdcbdd40
Máximo Fernández <maximo03f@gmail.com>
2025-01-18 17:43:44 -0300

    se pueden ver las followlists

--- Notify/albums/serializers.py
+++ Notify/albums/serializers.py
@@ -4,6 +4,14 @@ from .models import Album
 
 
 class AlbumSerializer(serializers.ModelSerializer):
+    titulo = serializers.CharField(source="title")
+    etiquetas = serializers.CharField(source="tags")
+    fechaLanzamiento = serializers.CharField(source="releaseDate")
+    duracion = serializers.IntegerField(source="length")
+    foto = serializers.CharField(source="cover")
+    reproducciones = serializers.IntegerField(source="playcount")
+    artista = serializers.CharField(source="autor.name")
+
     class Meta:
         model = Album
-        fields = ['title', 'tags','releaseDate','length','cover','playcount','autor']
+        fields = ['titulo', 'etiquetas','fechaLanzamiento','duracion','foto','reproducciones','artista']
--- Notify/followlists/views.py
+++ Notify/followlists/views.py
@@ -4,16 +4,19 @@ from .models import Follow
 from albums.models import Album
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from albums.serializers import AlbumSerializer
 
 @api_view(['GET'])
 def mostrarFL(request, nombreUsuario):
     userActual = Usuario.objects.filter(username = nombreUsuario).first()
     followsUser = Follow.objects.all().filter(usuario = userActual.id)
     listaAlbumes = []
+
+
     for f in followsUser:
         albumAux = Album.objects.filter(id = f.album.id).first()
-        dicA = dict(albumAux)
-        print(dicA)
-        listaAlbumes.append(dicA)
+        s = AlbumSerializer(albumAux)
+        listaAlbumes.append(s.data)
+        break
     return  Response(listaAlbumes)
 
