dee2f2b423a56f2534f25449d61f945b62b461c1
mateo <mateoedgardogomez@gmail.com>
2025-11-06 19:19:49 -0300

    Empezado back y front

--- Notify/usuarios/admin.py
+++ Notify/usuarios/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-#from django.contrib.auth.admin import UserAdmin
+
+# from django.contrib.auth.admin import UserAdmin
 from .models import Usuario
 
-#admin.site.register(Usuario, UserAdmin)
+# admin.site.register(Usuario, UserAdmin)
 admin.site.register(Usuario)
